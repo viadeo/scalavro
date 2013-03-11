2e493167d2a54b9afb5da05f977033d59e05fd77
Connor Doyle <connor_doyle@gensler.com>
2013-03-11 16:31:10 -0500

    Refactored AvroOrder & concrete subclasses a bit -- now contained within the AvroOrder companion object and have slightly more brief names.

--- src/main/scala/com/gensler/scalavro/avroOrder.scala
+++ src/main/scala/com/gensler/scalavro/avroOrder.scala
@@ -1,9 +1,9 @@
 package com.gensler.scalavro
 
-trait AvroOrder {
-  def value(): String
-}
+trait AvroOrder { def value(): String }
 
-case object AvroAscending extends AvroOrder { val value = "ascending" }
-case object AvroDescending extends AvroOrder { val value = "descending" }
-case object AvroIgnore extends AvroOrder { val value = "ignore" }
+object AvroOrder {
+  case object Ascending extends AvroOrder { val value = "ascending" }
+  case object Descending extends AvroOrder { val value = "descending" }
+  case object Ignore extends AvroOrder { val value = "ignore" }
+}
