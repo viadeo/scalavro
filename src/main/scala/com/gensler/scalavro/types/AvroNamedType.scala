3d7542db504852c0d0ed4dc40195ec24eb2e5451
Connor Doyle <connor_doyle@gensler.com>
2013-03-14 10:47:54 -0500

    Fixed a misleading docblock comment which incorrectly indicated that the three AvroNamedType subclasses should be ,  and .  In fact, the correct three subclasses are , , and .

--- src/main/scala/com/gensler/scalavro/types/AvroNamedType.scala
+++ src/main/scala/com/gensler/scalavro/types/AvroNamedType.scala
@@ -2,7 +2,7 @@ package com.gensler.scalavro.types
 
 /**
   * Marker trait for "named types".  As of version 1.7.4 of the Avro
-  * specification, the named types are `Record`, `Union`, and `Fixed`.
+  * specification, the named types are `Record`, `Enum`, and `Fixed`.
   */
 trait AvroNamedType[T] extends AvroComplexType[T] {
   def name: String
