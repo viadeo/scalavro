58b901de4a5f86f5816011b865d3d6b89e07f81c
Connor Doyle <connor_doyle@gensler.com>
2013-04-23 13:48:36 -0500

    Added custom formatting preferences to scalariform in the build definition.

--- project/Build.scala
+++ project/Build.scala
@@ -1,15 +1,31 @@
 import sbt._
 import Keys._
 
+import com.typesafe.sbt.SbtScalariform.scalariformSettings
 import scalariform.formatter.preferences._
 
 object ScalavroBuild extends Build {
 
+  ScalariformKeys.preferences := FormattingPreferences()
+    .setPreference(IndentWithTabs, false)
+    .setPreference(IndentSpaces, 2)
+    .setPreference(DoubleIndentClassDeclaration, false)
+    .setPreference(MultilineScaladocCommentsStartOnFirstLine, false)
+    .setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk, true)
+    .setPreference(PreserveDanglingCloseParenthesis, false)
+    .setPreference(AlignSingleLineCaseStatements, true)
+    .setPreference(PreserveSpaceBeforeArguments, true)
+    .setPreference(SpaceBeforeColon, false)
+    .setPreference(SpaceInsideBrackets, false)
+    .setPreference(SpaceInsideParentheses, false)
+    .setPreference(SpacesWithinPatternBinders, true)
+    .setPreference(FormatXml, true)
+
+
   lazy val root = Project(
     id = "scalavro",
-    base = file("."),
-    settings = scalariformSettings
-  ) aggregate(
+    base = file(".")
+  ) settings(scalariformSettings: _*) aggregate(
     core,
     io,
     util
@@ -18,14 +34,14 @@ object ScalavroBuild extends Build {
   lazy val core = Project(
     id = "core",
     base = file("core")
-  ) dependsOn(
+  ) settings(scalariformSettings: _*) dependsOn(
     util
   )
 
   lazy val io = Project(
     id = "io",
     base = file("io")
-  ) dependsOn(
+  ) settings(scalariformSettings: _*) dependsOn(
     core,
     util
   )
@@ -33,6 +49,6 @@ object ScalavroBuild extends Build {
   lazy val util = Project(
     id = "util",
     base = file("util")
-  )
+  ) settings(scalariformSettings: _*)
 
 }
