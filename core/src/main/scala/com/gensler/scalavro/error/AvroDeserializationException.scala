c53556b2ddea267c94a214adc5aff5de2133be0d
Connor Doyle <connor_doyle@gensler.com>
2013-06-24 09:37:40 -0500

    Added a `detailedMessage` parameter to custom error classes AvroDeserializtionException and AvroSerializationException for better failure output.

--- core/src/main/scala/com/gensler/scalavro/error/AvroDeserializationException.scala
+++ core/src/main/scala/com/gensler/scalavro/error/AvroDeserializationException.scala
@@ -2,8 +2,13 @@ package com.gensler.scalavro.error
 
 import scala.reflect.runtime.universe._
 
-class AvroDeserializationException[T: TypeTag](cause: java.lang.Throwable = null)
-  extends Exception(
-    "A problem occurred while attempting to deserialize a value of type [" + typeOf[T] + "].",
-    cause
-  )
+class AvroDeserializationException[T: TypeTag](
+  cause: java.lang.Throwable = null,
+  detailedMessage: String = "") extends Exception(
+  "A problem occurred while attempting to deserialize a value of type [" + typeOf[T] + "]." +
+    (
+      if (detailedMessage.nonEmpty) "\n" + detailedMessage
+      else ""
+    ),
+  cause
+)
--- core/src/main/scala/com/gensler/scalavro/error/AvroSerializationException.scala
+++ core/src/main/scala/com/gensler/scalavro/error/AvroSerializationException.scala
@@ -2,9 +2,16 @@ package com.gensler.scalavro.error
 
 import scala.reflect.runtime.universe._
 
-class AvroSerializationException[T: TypeTag](obj: T, cause: java.lang.Throwable = null)
-  extends Exception(
-    "A problem occurred while attempting to serialize a value of type [" + typeOf[T] + "].\n" +
-      "Attempted to serialize the value: " + obj.toString,
-    cause
-  )
+class AvroSerializationException[T: TypeTag](
+  obj: T,
+  cause: java.lang.Throwable = null,
+  detailedMessage: String = "")
+    extends Exception(
+      "A problem occurred while attempting to serialize a value of type [" + typeOf[T] + "].\n" +
+        "Attempted to serialize the value: " + obj.toString +
+        (
+          if (detailedMessage.nonEmpty) "\n" + detailedMessage
+          else ""
+        ),
+      cause
+    )
